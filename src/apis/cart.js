7f40acfe02465033c38344041f3c910ffcd7cd04
Bascii098 <2021210202@bupt.edu.cn>
2024-07-10 02:54:00 +0800

    购物车页数据填充

--- src/apis/cart.js
+++ src/apis/cart.js
@@ -1,12 +1,28 @@
 import http from '@/utils/http'
-export function getfoodsAPI({ name, num, price }) {
+const userInfo = localStorage.getItem('big-user')
+const token = JSON.parse(userInfo).token
+export function addcartAPI({ name, num, price, imgurl }) {
   return http({
     url: `/my/addcart`,
     method: 'POST',
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
     data: {
       name,
       num,
-      price
+      price,
+      imgurl
+    }
+  })
+}
+export function getcartAPI() {
+  return http({
+    url: '/my/getcart',
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
   })
 }
